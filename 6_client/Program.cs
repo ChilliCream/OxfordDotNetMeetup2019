a1eae97bd1c8643b7bac26305a91331610ac42ee
Michael Staib <michael@chillicream.com>
2019-11-11 21:01:10 +0100

    Added demos

---
+++ 6_client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StarWarsClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  
+        }
+    }
+}
